9a0bda4724f23f76c6f6918a32fec845b15369e5
kvoon <kvoon@duck.com>
2026-08-05 18:11:11 +0800

    perf: trail lerp 0.4 -> 0.3

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@ function startLerp() {
       rafId = 0
       return
     }
-    const next = t.y + (targetY - t.y) * 0.4
+    const next = t.y + (targetY - t.y) * 0.3
     t.y = Math.abs(targetY - next) < 0.5 ? targetY : next
     rafId = requestAnimationFrame(tick)
   }
