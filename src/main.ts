7de8c789c5781e8ad595713cbf0f4c3c614b1cbd
kvoon <kvoon@duck.com>
2026-08-05 18:41:59 +0800

    feat: MIT license, footer github link, dark-mode scrollbars
    - add MIT LICENSE (Kevin Kwong, 2026)
    - footer: github icon link to kvoon3/watch
    - global color-scheme css so native scrollbars follow theme
    - rename package to watch

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { createVaporApp, vaporInteropPlugin } from 'vue'
 import App from './App.vue'
 
 import 'virtual:uno.css'
+import './style.css'
 
 createVaporApp(App).use(vaporInteropPlugin).mount('#app')
